b5a958d21060271893bcd3be705aec4e0fb038fd
Ibrahim Alneami <d.alneami@gmail.com>
2026-09-18 23:39:26 +0300

    Add English letter and word content model

---
+++ src/data/content.js
@@ -0,0 +1,20 @@
+window.ENGLISH_LAB_CONTENT = {
+  letters: [
+    ['A','a','/æ/','apple'], ['B','b','/b/','ball'], ['C','c','/k/','cat'], ['D','d','/d/','dog'],
+    ['E','e','/ɛ/','egg'], ['F','f','/f/','fish'], ['G','g','/g/','goat'], ['H','h','/h/','hat'],
+    ['I','i','/ɪ/','igloo'], ['J','j','/dʒ/','jam'], ['K','k','/k/','kite'], ['L','l','/l/','lion'],
+    ['M','m','/m/','moon'], ['N','n','/n/','nest'], ['O','o','/ɒ/','octopus'], ['P','p','/p/','pig'],
+    ['Q','q','/kw/','queen'], ['R','r','/r/','rain'], ['S','s','/s/','sun'], ['T','t','/t/','top'],
+    ['U','u','/ʌ/','umbrella'], ['V','v','/v/','van'], ['W','w','/w/','web'], ['X','x','/ks/','fox'],
+    ['Y','y','/j/','yes'], ['Z','z','/z/','zebra']
+  ].map(([upper, lower, phoneme, example]) => ({ upper, lower, phoneme, example })),
+  words: [
+    { word: 'CAT', hint: 'A small animal that says “meow”.' },
+    { word: 'SUN', hint: 'It shines in the sky during the day.' },
+    { word: 'DOG', hint: 'A common pet that can bark.' },
+    { word: 'MAP', hint: 'It helps you find places.' },
+    { word: 'FISH', hint: 'An animal that swims in water.' },
+    { word: 'BOOK', hint: 'You read it.' },
+    { word: 'MOON', hint: 'You can often see it at night.' }
+  ]
+};
