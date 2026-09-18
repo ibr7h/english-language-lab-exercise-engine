82c952b678f4df1c2861723b7c178e5ed5a87ae6
Ibrahim Alneami <d.alneami@gmail.com>
2026-09-18 23:39:52 +0300

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'english-language-lab-v1';
+const ASSETS = [
+  './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './src/data/content.js',
+  './assets/icons/icon-192.png', './assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
